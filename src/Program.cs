8a2362cac02688bdd0412aaf4f60cef01abaaa56
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2022-12-11 23:59:40 +0200

    Fix

--- src/Program.cs
+++ src/Program.cs
@@ -2,22 +2,7 @@
 namespace FinTrackApi
 {
 
-    using FinTrackApi.Data;
-    using FinTrackApi.Data.Models;
-    using FinTrackApi.Data.Seeding;
-    using FinTrackApi.Infrastructure;
     using FinTrackApi.Infrastructure.Extensions;
-    using FinTrackApi.Infrastructure.Services;
-    using FinTrackApi.Services;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.IdentityModel.Tokens;
-    using Microsoft.OpenApi.Models;
-    using System.Net.NetworkInformation;
-
-    using System.Text;
 
     public class Program
     {
@@ -27,15 +12,6 @@ namespace FinTrackApi
             builder.Services.RegisterServices(builder.Configuration);
            
             var app = builder.Build();
-
-            using (var serviceScope = app.Services.CreateScope())
-            {
-                var dbContext = serviceScope.ServiceProvider.GetRequiredService<FinTrackApiDbContext>();
-                var roleSeed = new RoleSeeder();
-                roleSeed.Seed(dbContext, serviceScope.ServiceProvider);
-                dbContext.SaveChanges();
-                dbContext.Database.Migrate();
-            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -47,7 +23,6 @@ namespace FinTrackApi
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
