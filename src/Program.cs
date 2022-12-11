bf993d26bc3105d27a803b9ab1335d2d13aeac6a
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2022-12-11 23:37:29 +0200

    Added ServiceCollectionExtension => refactored services in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,7 @@ namespace FinTrackApi
     using FinTrackApi.Data.Models;
     using FinTrackApi.Data.Seeding;
     using FinTrackApi.Infrastructure;
+    using FinTrackApi.Infrastructure.Extensions;
     using FinTrackApi.Infrastructure.Services;
     using FinTrackApi.Services;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -14,7 +15,6 @@ namespace FinTrackApi
     using Microsoft.Extensions.Configuration;
     using Microsoft.IdentityModel.Tokens;
     using Microsoft.OpenApi.Models;
-    using System.Net.NetworkInformation;
     using System.Text;
 
     public class Program
@@ -22,90 +22,9 @@ namespace FinTrackApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+           
+            builder.Services.RegisterServices(builder.Configuration);
 
-            
-        // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-     
-            builder.Services.AddDbContext<FinTrackApiDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            //services
-            builder.Services.AddScoped<ISeeder, RoleSeeder>();
-            builder.Services.AddScoped<IIdentityService, IdentityService>();
-            builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
-
-
-            builder.Services
-                .AddIdentity<User, IdentityRole>(options =>
-                {
-                    options.Password.RequiredLength = 3;
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.User.RequireUniqueEmail = true;
-                    options.ClaimsIdentity.RoleClaimType.IsNormalized();
-                })
-            .AddEntityFrameworkStores<FinTrackApiDbContext>();
-            AppSettings settings = new();
-
-            var applicationSettingsConfiguration = builder.Configuration.GetSection("ApplicationSettings");
-            builder.Services.Configure<AppSettings>(applicationSettingsConfiguration);
-            var myAppSecret = applicationSettingsConfiguration.Get<AppSettings>();
-
-            var key = Encoding.ASCII.GetBytes(myAppSecret.Secret);
-
-            builder.Services
-                .AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-
-                    };
-                });
-
-            //swagger
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Scheme = "bearer",
-                    Description = "Please insert Jwt token",
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-                  {
-                        new OpenApiSecurityScheme
-                        {
-                           Reference = new OpenApiReference
-                           {
-                             Type = ReferenceType.SecurityScheme,
-                             Id = "Bearer"
-                           }
-                        },
-                            new string[] { }
-                  }
-                });
-            });
             var app = builder.Build();
 
             using (var serviceScope = app.Services.CreateScope())
